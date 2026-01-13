8b9b5a0cdeb277734477e3349b3ab0f1aba9fd53
Norak Hakobyan <norak.hak98@gmail.com>
2026-01-13 16:39:24 +0400

    chore: setup project + add AppWrapper

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import AppWrapper from "./components/AppWrapper/AppWrapper";
+
+function App() {
+    return <AppWrapper />
+}
+
+export default App;
---
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AppWrapper.css"
+
+export default function AppWrapper() {
+    return <div className="app-wrapper">
+        <header className="header">
+            <h1>Geo Pokemon</h1>
+            <button>Menu</button>
+        </header>
+    </div>
+}
